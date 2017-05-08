d7c8b327e8b83a48865ef9518abf08c06757c02d
sagar setia <ssetia.nitk@gmail.com>
2017-05-08 18:59:30 +0000

    speller initialised

---
+++ speller/dictionary.c
@@ -0,0 +1,53 @@
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dictionary.h"
+
+/** 
+ * Implementing a trie datastructure
+ */
+
+
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word)
+{
+    // TODO
+ return true;
+    
+     
+}
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary)
+{
+    // TODO
+
+   return true;
+    
+}
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void)
+{
+    // TODO
+  
+}
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void)
+{
+    // TODO
+   
+    
+}
